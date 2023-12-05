addac78fb94f84836a630b5807922cc550703a85
haji09 <hajibaba005@gmail.com>
2023-12-05 16:30:50 +0530

    update

--- components/works/FlickMaps.tsx
+++ components/works/FlickMaps.tsx
@@ -14,7 +14,7 @@ const FlickMaps = () => {
         <span className="text-textGreen tracking-wide">@FlickMaps</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
-        Jan 2022 - Aug 2023
+        March 2021 - Dec 2021
       </p>
       <ul className="mt-6 flex flex-col gap-3">
         <li className="text-base flex gap-2 text-textDark">
